9ba994126d73e36252792f5f19877a4bcdb18d75
Papa Smurf <chris@violet.co>
2022-05-13 13:54:49 +0100

    fixed tasks index

--- tasks/index.ts
+++ tasks/index.ts
@@ -1,17 +1,8 @@
 const deployExtendable = require("./deploy/extendable");
 const deployExtension = require("./deploy/extension");
-const deployFactory = require("./deploy/factory");
-const deployLibrary = require("./deploy/library");
-const deployRegistry = require("./deploy/registry");
-
-const libraryEnlist = require("./library/enlist");
-const libraryDelist = require("./library/delist");
-const libraryGetters = require("./library/getter");
-
-const registryRegister = require("./registry/register");
-const registryDeregister = require("./registry/deregister");
-const registryGetter = require("./registry/getter");
-const registrySetter = require("./registry/setter");
+const deployBase = require("./deploy/erc721");
+const deployMetadata = require("./deploy/erc721Metadata");
+const deployEnumerable = require("./deploy/erc721Enumerable");
 
 const accounts = require("./accounts");
 
@@ -19,14 +10,7 @@ export {
     accounts,
     deployExtendable,
     deployExtension,
-    deployFactory,
-    deployLibrary,
-    deployRegistry,
-    libraryEnlist,
-    libraryDelist,
-    libraryGetters,
-    registryRegister,
-    registryDeregister,
-    registryGetter,
-    registrySetter
+    deployBase,
+    deployMetadata,
+    deployEnumerable
 }
