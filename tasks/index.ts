4657f0e67f1abdcc8ea65609523fbacab123efe1
ra-phael <10075759+ra-phael@users.noreply.github.com>
2022-06-21 19:06:34 +0100

    fix(): add missing task import

--- tasks/index.ts
+++ tasks/index.ts
@@ -4,13 +4,7 @@ const deployBase = require("./deploy/erc721");
 const deployMetadata = require("./deploy/erc721Metadata");
 const deployEnumerable = require("./deploy/erc721Enumerable");
 
+const extend = require("./extend");
 const accounts = require("./accounts");
 
-export {
-    accounts,
-    deployExtendable,
-    deployExtension,
-    deployBase,
-    deployMetadata,
-    deployEnumerable
-}
+export { accounts, deployExtendable, deployExtension, extend, deployBase, deployMetadata, deployEnumerable };
