e8bd164403c730428db725fbcb22a834010d8558
Papa Smurf <chris@violet.co>
2022-08-30 14:06:31 +0100

    feat: updated SetTokenURILogic

--- contracts/extensions/metadata/setTokenURI/IBasicSetTokenURILogic.sol
+++ contracts/extensions/metadata/setTokenURI/IBasicSetTokenURILogic.sol
@@ -1,6 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+import "@violetprotocol/extendable/extensions/InternalExtension.sol";
+
 interface IBasicSetTokenURILogic {
     /**
      * @dev See {ERC721URIStorage-_setTokenURI}.
@@ -11,4 +13,28 @@ interface IBasicSetTokenURILogic {
      * @dev See {ERC721URIStorageMock-_setBaseURI}.
      */
     function setBaseURI(string memory _tokenURI) external;
+}
+
+abstract contract BasicSetTokenURIExtension is IBasicSetTokenURILogic, Extension {
+    /**
+     * @dev see {IExtension-getSolidityInterface}
+     */
+    function getSolidityInterface() public pure virtual override returns (string memory) {
+        return
+            "function setTokenURI(uint256 tokenId, string memory _tokenURI) external;\n"
+            "function setBaseURI(string memory _tokenURI) external;\n";
+    }
+
+    /**
+     * @dev see {IExtension-getInterface}
+     */
+    function getInterface() public virtual override returns (Interface[] memory interfaces) {
+        interfaces = new Interface[](1);
+
+        bytes4[] memory functions = new bytes4[](2);
+        functions[0] = IBasicSetTokenURILogic.setTokenURI.selector;
+        functions[1] = IBasicSetTokenURILogic.setBaseURI.selector;
+
+        interfaces[0] = Interface(type(IBasicSetTokenURILogic).interfaceId, functions);
+    }
 }
